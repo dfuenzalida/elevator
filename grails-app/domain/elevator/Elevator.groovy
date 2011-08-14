5718f0d69f43567d0b1e336829e32defa4b047f9
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2011-08-13 20:51:42 -0400

    Added basic Elevator domain class which extends Gestate

---
+++ grails-app/domain/elevator/Elevator.groovy
@@ -0,0 +1,54 @@
+package elevator
+
+import gestate.*
+
+class Elevator extends Gestate {
+
+    def gestateService
+    
+    static constraints = {
+    }
+    
+    // Simple domain class for an elevator with 4 floors
+    // The floors are: Ground <-> 2nd Floor <-> 3rd Floor <-> 4th Floor
+    
+    static _elevatorStates = [
+    
+       ground:[
+            verb: 'Ground', 
+            destinations: [
+                second:{ println "Going up from Ground to 2nd" },
+            ]],
+    
+       second:[
+            verb: '2nd floor', 
+            destinations: [
+                ground:{ println "Going up from 2nd to 3rd" },
+                third:{ println "Going down to Ground from 2nd" },
+            ]],
+    
+       third:[
+            verb: '3rd floor', 
+            destinations: [
+                fourth:{ println "Going up from 3rd to 4th" },
+                second:{ println "Going down from 2nd to 3rd" },
+            ]],
+    
+       fourth:[
+            verb: '4th floor', 
+            destinations: [
+                third:{ println "Going down from 4th to 3rd" },
+            ]],
+    
+    ]
+    
+    public Elevator(){
+        this._states = _elevatorStates
+        this.state = _elevatorStates.collect{it.key}[0]
+    }
+    
+    boolean changeStateTo(String state) {
+        return gestateService.changeTo(this, state)
+    }
+    
+}
---
+++ test/unit/elevator/ElevatorTests.groovy
@@ -0,0 +1,17 @@
+package elevator
+
+import grails.test.*
+
+class ElevatorTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
