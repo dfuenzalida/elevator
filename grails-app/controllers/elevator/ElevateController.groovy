b4b138c5d7ee6a0cde5d136c3316d703e091e245
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2011-08-15 20:32:10 -0400

    Formatting the controller code

--- grails-app/controllers/elevator/ElevateController.groovy
+++ grails-app/controllers/elevator/ElevateController.groovy
@@ -3,23 +3,13 @@ package elevator
 class ElevateController {
 
     def index = {
-    
-        // remove any old elevator instance
-        // Elevator.findAll().each { it.delete() }
-        
-        // Create and save a new instance
-        // Elevator elevator = new Elevator()
-        // elevator.save()
-        
         def elevators = Elevator.findAll()
-    
         [elevators: elevators]
     }
     
     def create = {
         Elevator elevator = new Elevator()
         elevator.save()
-    
         redirect(action: index)
     }
     
