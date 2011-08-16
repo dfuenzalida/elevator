6b8473753fd8db389f521d8cb60bd6291dfea050
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2011-08-15 21:21:52 -0400

    Fixed verbs so they don't have spaces (for HTML ids)

--- grails-app/domain/elevator/Elevator.groovy
+++ grails-app/domain/elevator/Elevator.groovy
@@ -21,21 +21,21 @@ class Elevator extends Gestate {
             ]],
     
        second:[
-            verb: '2nd floor', 
+            verb: '2nd-floor', 
             destinations: [
                 ground:{ println "Going up from 2nd to 3rd" },
                 third:{ println "Going down to Ground from 2nd" },
             ]],
     
        third:[
-            verb: '3rd floor', 
+            verb: '3rd-floor', 
             destinations: [
                 fourth:{ println "Going up from 3rd to 4th" },
                 second:{ println "Going down from 2nd to 3rd" },
             ]],
     
        fourth:[
-            verb: '4th floor', 
+            verb: '4th-floor', 
             destinations: [
                 third:{ println "Going down from 4th to 3rd" },
             ]],
